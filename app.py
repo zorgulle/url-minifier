69c62cbfeabcc79fa751d85e00069f426749cf4f
Adrien <winy95battle@gmail.com>
2018-07-28 20:25:10 +0200

    Add StringEncoder

--- app.py
+++ app.py
@@ -30,6 +30,13 @@ class UrlDAO:
         return session.query(Urls).filter_by(id=int(url_id)).first()
 
 
+class StringEncoding:
+    @staticmethod
+    def encode(to_encode):
+        return to_encode.encode("utf-8")
+    @staticmethod
+    def decode(to_decode):
+        return to_decode.decode("utf-8")
 
 class Encoder:
     @staticmethod
@@ -43,11 +50,19 @@ class Encoder:
 class Base64EncoderStrategy(Encoder):
     @staticmethod
     def encode(to_encode):
-        return base64.standard_b64encode(to_encode.encode("utf-8")).decode("utf-8")
+        encoded_string = StringEncoding.encode(to_encode)
+        base_64_encoded_id = base64.standard_b64encode(encoded_string)
+        decoded_string = StringEncoding.decode(base_64_encoded_id)
+
+        return decoded_string
 
     @staticmethod
     def decode(to_decode):
-        return base64.standard_b64decode(to_decode.encode("utf-8")).decode("utf-8")
+        encoded_string = StringEncoding.encode(to_decode)
+        base_64_decoded_id = base64.standard_b64decode(encoded_string)
+        decoded_string = StringEncoding.decode(base_64_decoded_id)
+
+        return decoded_string
 
 
 class Minifyer:
