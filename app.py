69c012d0de69ba0ec7603e15bd51ee59c39b7f23
Adrien <winy95battle@gmail.com>
2018-07-28 19:27:02 +0200

    Base code but not clean

--- app.py
+++ app.py
@@ -1,2 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine("sqlite:///toto.sql")
+session = sessionmaker(bind=engine)()
+
+Base = declarative_base()
+
+class Urls(Base):
+    __tablename__ = "Urls"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String)
+
 def minify(url: str) -> str:
-    return "MQ=="
+    import base64
+
+    url = Urls(url=url)
+    session.add(url)
+    session.flush()
+
+    x = base64.standard_b64encode(str(url.id).encode("utf-8"))
+
+    session.commit()
+
+    return x.decode("utf-8")
+
+def deminify(url: str) -> int:
+    import base64
+
+    x = base64.standard_b64decode(url.encode("utf-8"))
+    url = session.query(Urls).filter_by(id=int(x)).first()
+
+    return url.url
+
+if __name__ == "__main__":
+    x = minify("www.google2.com")
+    print(x)
+    x = deminify(x)
+    print(x)
