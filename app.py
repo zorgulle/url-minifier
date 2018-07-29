5139dbdeaa88143e99426db7c182d9bba442e768
Adrien <winy95battle@gmail.com>
2018-07-29 19:49:12 +0200

    Add argument into main module

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import argparse
+
 from encoder.encoder import Base64EncoderStrategy
 from minifyer.minifyer import Minifyer
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Minify url")
+    parser.add_argument("--url", type=str, required=True, help="Url to minify")
+    parser.add_argument("--action", type=str, default="minify", choices=["minify", "deminify"])
+    args = parser.parse_args()
+
     minifyier = Minifyer(Base64EncoderStrategy())
 
-    x = minifyier.minify("www.google2.com")
-    print(x)
-    x = minifyier.deminify(x)
-    print(x)
+    actions_mapping = {
+        "minify": minifyier.minify,
+        "deminify": minifyier.deminify
+    }
+
+    result = actions_mapping[args.action](args.url)
+    print(result)
