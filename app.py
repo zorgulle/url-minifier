a55660b0f5dd51520d8295b17274f012d1d652a0
Adrien <winy95battle@gmail.com>
2018-07-30 21:29:53 +0200

    Add get_config stub function

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ import argparse
 from encoder.encoder import Base64EncoderStrategy
 from minifyer.minifyer import Minifyer
 
-if __name__ == "__main__":
+
+def get_arguments():
+    global args
     parser = argparse.ArgumentParser("Minify url")
     parser.add_argument("--url", type=str, required=True, help="Url to minify")
     parser.add_argument("--action", type=str, default="minify", choices=["minify", "deminify"])
+    parser.add_argument("--config", type=str, default="dev.json", help="Config file")
     args = parser.parse_args()
+
+def get_config(config_type):
+    return {}
+
+if __name__ == "__main__":
+    get_arguments()
+
+    config = get_config(args.config)
 
     minifyier = Minifyer(Base64EncoderStrategy())
 
